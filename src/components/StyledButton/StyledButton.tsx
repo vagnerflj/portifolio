08f652caa380774818eecd74303750eae1f9bf97
Vagnerflj <116043257+vagnerflj@users.noreply.github.com>
2024-10-15 09:21:31 -0300

    feat: creat Button onclick

--- src/components/StyledButton/StyledButton.tsx
+++ src/components/StyledButton/StyledButton.tsx
@@ -4,8 +4,9 @@ import React, {ReactNode} from "react";
 
 interface StyledButtonProps {
     children: ReactNode;
+    onClick: () => void
 }
-const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
+const StyledButton: React.FC<StyledButtonProps> = ({children, onClick}) => {
 
     const StyledButton = styled("button")(({ theme }) => ({
         backgroundColor: "transparent",
@@ -26,7 +27,7 @@ const StyledButton: React.FC<StyledButtonProps> = ({children}) => {
 
     return (
         <>
-            <StyledButton>
+            <StyledButton onClick={onClick}>
                 {children}
             </StyledButton>
         </>
--- src/pages/Home/sections/Hero/Hero.tsx
+++ src/pages/Home/sections/Hero/Hero.tsx
@@ -53,14 +53,14 @@ const Hero = () => {
                         <Grid container display="flex" justifyContent="center" spacing={3} pt={3}>
                             <Grid item xs={12} md={4} display="flex" justifyContent="center">
 
-                                <StyledButton>
+                                <StyledButton onClick={()=> console.log("download")}>
                                     <DownloadIcon/>
                                     <Typography>
                                         Download CV</Typography>
                                 </StyledButton>
                             </Grid>
                             <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                <StyledButton>
+                                <StyledButton onClick={()=> console.log("contact")}>
                                     <EmailIcon/>
                                     <Typography>
                                     Contact me</Typography>
